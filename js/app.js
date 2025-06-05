c271b290e8561adf9b793f3a66e220583cf354a6
JackBadeaux <jack.badeaux@mscoding.org>
2025-06-04 21:25:59 -0500

    added location select

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// ! Characters
 const mage = {
     name: "Mage",
     hp: 100,
@@ -8,7 +9,7 @@ const warrior = {
     name: "Warrior",
     hp: 150,
     dmg: 12,
-    defence: 5,
+    defence: 4,
 }
 const rogue = {
     name: "Rogue",
@@ -19,21 +20,52 @@ const rogue = {
 const player = {}
 const heroButton = document.querySelectorAll(".hero-choice")
 const statInfo = document.getElementById("statInfo")
-// const div = document.createElement("div")
+
+const locationButton = document.querySelectorAll(".location-button")
 
 
+
+// ! Character Select
 heroButton.forEach(button => {
     button.addEventListener("click", () => {
         if (button.id === "mage") {
             Object.assign(player, mage);
-            statInfo.textContent = Object.entries(player)
+            statInfo.innerHTML = `
+            <div>Name: ${player.name}</div>
+            <div>❤️: ${player.hp}</div>
+            <div>⚔️: ${player.dmg}</div>
+            <div>🛡️: ${player.defence}</div>`;
+
         } else if (button.id === "warrior") {
             Object.assign(player, warrior);
-            statInfo.textContent = Object.entries(player)
+                       statInfo.innerHTML = `
+            <div>Name: ${player.name}</div>
+            <div>❤️: ${player.hp}</div>
+            <div>⚔️: ${player.dmg}</div>
+            <div>🛡️: ${player.defence}</div>`;
         } else if (button.id === "rogue") {
             Object.assign(player, rogue);
-            statInfo.textContent = Object.entries(player)
+            statInfo.innerHTML = `
+            <div>Name: ${player.name}</div>
+            <div>❤️: ${player.hp}</div>
+            <div>⚔️: ${player.dmg}</div>
+            <div>🛡️: ${player.defence}</div>`;
         }
-        document.getElementById("characterSelectPage").style.display= "none"
+        document.getElementById("characterSelectPage").style.display = "none"
+    });
+});
+// ! Location Select 
+locationButton.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.id === "lake") {
+            document.body.style.backgroundColor = "blue";
+            document.documentElement.style.backgroundColor = "blue";
+            document.querySelector("main").style.backgroundColor = "blue";
+        } else if (button.id === "forest") {
+            document.body.style.backgroundColor = "green";
+            document.documentElement.style.backgroundColor = "green";
+            document.querySelector("main").style.backgroundColor = "green";
+        }
+        document.getElementById("locationSelectPage").style.display = "none"
     });
 });
